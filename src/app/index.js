301647b0f9735ab6089b5c48a27b96776f467b28
GORDELAS08 <159555583+GORDELAS08@users.noreply.github.com>
2024-08-07 12:07:55 +0000

    [feat]expo-router

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>aplicativo pronto para usar</Text>
+        <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
