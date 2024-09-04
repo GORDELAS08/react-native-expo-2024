9fe5e3a4ef695a4d88130245f179ce6901d20852
GORDELAS08 <159555583+GORDELAS08@users.noreply.github.com>
2024-09-04 10:47:24 +0000

    [feat] Protected Routes

---
+++ src/app/(protected)/index.js
@@ -0,0 +1,9 @@
+import { FlipInEasyX } from "react-native-reanimated"
+
+export default function Home(){
+    return{
+        <View style={{ flex: 1, justifyContent}}>
+            <Text>Home</Text>
+        </View>
+    };
+}
